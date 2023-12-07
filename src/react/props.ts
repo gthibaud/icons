bc16fadadc709aa9d72b5f1dd46cbdadfbd2fbd4
Grégoire Thibaud <gregoire.thibo@gmail.com>
2023-12-07 15:39:40 -0800

    add new rules

--- src/react/props.ts
+++ src/react/props.ts
@@ -5,6 +5,7 @@ export const addPropsToSVG = (iconSVGSource: string, iconName: string): string =
     iconSVGSource = iconSVGSource.replace(/fill="#000000"/g, 'fill="inherit"');
     iconSVGSource = iconSVGSource.replace(/fill="none"/g, 'fill={color || "inherit"}');
     iconSVGSource = iconSVGSource.replace(/fill-rule/g, 'fillRule');
+    iconSVGSource = iconSVGSource.replace(/fill-opacity/g, 'fillOpacity');
     iconSVGSource = iconSVGSource.replace(/clip-rule/g, 'clipRule');
     iconSVGSource = iconSVGSource.replace(/<svg /g, `<svg className={className} style={{ display:'inline', margin: '0px 0.4ch 1px 0.4ch', ...style}} aria-label={arialLabel || "${iconName} icon"} `);
     return iconSVGSource;
